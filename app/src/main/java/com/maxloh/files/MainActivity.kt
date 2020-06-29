bb1f1bbd2f96bdf61baed0b59c80e844ea95d362
maxloh <35831069+maxloh@users.noreply.github.com>
2020-06-30 03:52:49 +0800

    Support Andoid 10 (only)

--- app/src/main/java/com/maxloh/files/MainActivity.kt
+++ app/src/main/java/com/maxloh/files/MainActivity.kt
@@ -1,23 +1,24 @@
 package com.maxloh.files
 
-import android.content.Intent
 import android.content.ComponentName
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(
-            Intent()
-                .setComponent(
-                    ComponentName(
-                        "com.android.documentsui",
-                        "com.android.documentsui.files.FilesActivity"
-                    )
-                )
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.component = ComponentName(
+            "com.google.android.documentsui",
+            "com.android.documentsui.files.FilesActivity"
         )
+        intent.data = Uri.parse("content://com.android.externalstorage.documents/root/primary")
+        intent.flags = FLAG_ACTIVITY_NEW_TASK or FLAG_ACTIVITY_CLEAR_TOP
+        startActivity(intent)
         finish()
     }
 }
