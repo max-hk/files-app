0a3f716039f12d9a719599e9a1fe360699f8f4fe
maxloh <35831069+maxloh@users.noreply.github.com>
2020-06-30 05:28:39 +0800

    Support Andoid 10 (only) (2)

--- app/src/main/java/com/maxloh/files/MainActivity.kt
+++ app/src/main/java/com/maxloh/files/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.maxloh.files
 
+import android.app.Activity
 import android.content.ComponentName
 import android.content.Intent
 import android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP
 import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.net.Uri
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val intent = Intent(Intent.ACTION_VIEW)
