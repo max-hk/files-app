d22d4c7361b290d06582a0b05a4180fce5bb4ece
maxloh <35831069+maxloh@users.noreply.github.com>
2020-06-30 03:40:17 +0800

    Temp

--- app/src/main/java/com/maxloh/files/MainActivity.kt
+++ app/src/main/java/com/maxloh/files/MainActivity.kt
@@ -4,20 +4,29 @@ import android.content.Intent
 import android.content.ComponentName
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.provider.DocumentsContract
 
 class MainActivity : AppCompatActivity() {
 
+    val ACTION_BROWSE_DOCUMENT_ROOT = "android.provider.action.MANAGE_DOCUMENT"
+    private val DOCUMENT_AUTHORITY = "com.android.externalstorage.documents"
+    private val DOCUMENT_ROOT_PRIMARY_EMULATED = "primary"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(
-            Intent()
-                .setComponent(
-                    ComponentName(
-                        "com.android.documentsui",
-                        "com.android.documentsui.files.FilesActivity"
-                    )
-                )
+        val uri = DocumentsContract.buildRootUri(
+            DOCUMENT_AUTHORITY,
+            DOCUMENT_ROOT_PRIMARY_EMULATED
         )
+        val intent = Intent(ACTION_BROWSE_DOCUMENT_ROOT)
+        intent.addCategory(Intent.CATEGORY_DEFAULT)
+        intent.data = uri
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
         finish()
     }
 }
